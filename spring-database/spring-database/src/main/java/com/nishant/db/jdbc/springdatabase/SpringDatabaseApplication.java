9d2dbc32744d86bdf90a0f9e7134547d6894e9be
Nishant Naveen <nshtnvn@gmail.com>
2017-12-11 21:49:36 +0530

    Spring Database Basics

---
+++ spring-database/spring-database/src/main/java/com/nishant/db/jdbc/springdatabase/SpringDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.nishant.db.jdbc.springdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseApplication.class, args);
+	}
+}
